ac95d8f511564aabb1207d33f4204fe63f8400b4
NewbiCoder <74412799+VidicanSerban@users.noreply.github.com>
2022-11-25 15:51:26 +0200

    Update RegisterViewModel.kt

--- app/src/main/java/com/example/eventhub/feature_onboarding/onboarding_viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/eventhub/feature_onboarding/onboarding_viewmodel/RegisterViewModel.kt
@@ -13,19 +13,57 @@ class RegisterViewModel(): ViewModel() {
     private val passwordconfirmLiveData = MutableLiveData<String>()
 
     private fun validateEmail(): Boolean{
-
+        
+        lateinit var email: String
+        
+        if(email.isEmpty())
+        {
+            Toast.makeText(applicationContext, "Please enter email address", Toast.LENGTH_LONG).show()
+        }
+        else Toast.makeText(applicationContext, "Correct", Toast.LENGTH_SHORT).show()
         return true
     }
     private fun validateName(): Boolean{
-
+        
+        lateinit var name: String
+        
+        if(name.isEmpty())
+        {
+            Toast.makeText(applicationContext, "Please enter name", Toast.LENGTH_LONG).show()
+        }
+        else Toast.makeText(applicationContext, "Correct", Toast.LENGTH_SHORT).show()
         return true
     }
     private fun validatePassword(): Boolean{
-
+        
+        lateinit var password: String
+        
+        if(password.isEmpty())
+        {
+            Toast.makeText(applicationContext, "Please enter password", Toast.LENGTH_LONG).show()
+        }
+        else Toast.makeText(applicationContext, "Correct", Toast.LENGTH_SHORT).show()
         return true
     }
     private fun samePassword(): Boolean{
 
+        lateinit var passconfirm: String
+        lateinit var password: String
+        
+        if(passconfirm.isEmpty())
+        {
+            Toast.makeText(applicationContext, "Please enter password", Toast.LENGTH_LONG).show()
+            return false
+        }
+        else if(passconfirm.equals(password)){
+                    Toast.makeText(applicationContext, "Correct", Toast.LENGTH_SHORT).show()
+                    return true
+                }
+                else 
+                {
+                    Toast.makeText(applicationContext, "Password does not match", Toast.LENGTH_SHORT).show()
+                    return false
+                }
         return true
     }
 
